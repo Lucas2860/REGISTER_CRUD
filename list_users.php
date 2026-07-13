d8342c68ce91f63f8b59a4dd02df66459f080c62
Lucas2860 <lucas.santos286012@gmail.com>
2026-07-13 14:40:28 -0300

    :white_check_mark:Feita a pagina de listagem de usuários

--- list_users.php
+++ list_users.php
@@ -1,1 +1,35 @@
 <h1>Listar Usuários</h1>
+
+<?php
+    $sql = "SELECT * FROM usuarios";
+
+    $res = $conn->query($sql);
+
+    $qtd = $res->num_rows;
+    
+    if ($qtd > 0){
+        print "<table class='table table-houver table-striped table-bordered'>";
+            print "<tr>";
+            print "<th>#</th>";
+            print "<th>NOME</th>";
+            print "<th>E-MAIL</th>";
+            print "<th>DATA NASCIMENTO</th>"; 
+            print "<th>AÇÕES</th>"; 
+            print "</tr>";
+        while($row = $res->fetch_object()){
+            print "<tr>";
+            print "<td>".$row->id;
+            print "<td>".$row->nome;
+            print "<td>".$row->email;
+            print "<td>".$row->data_nasc; 
+            print "<td>
+                    <button class='btn btn-success'>EDITAR</button>
+                    <button class='btn btn-danger'>EXLUIR</button>
+                   </td>"; 
+            print "</tr>";
+        }
+        print "</table>";
+    }else{
+        print "<p class='alert alert-danger'>Não encontrou resutados!</p>";
+    }
+?>
