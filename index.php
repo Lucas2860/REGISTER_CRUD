243436761bc61f71185ca0968b1bf4bb11cf44b4
Lucas2860 <lucas.santos286012@gmail.com>
2026-07-13 10:59:49 -0300

    :sparkles:Adicionado Recursos do bootstrap

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>REGISTER CRUD</h1>
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cadastro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Opções
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="new_user.php">Novo Usuário</a></li>
+                    <li><a class="dropdown-item" href="list_users.php">Listar Usuários</a></li>
+                </ul>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col mt-5">
+                    <?php
+                        switch(@$_REQUEST["page"]){
+                            case "novo":
+                                include("new_user.php");
+                            break;
+                            case "listar":
+                                include("list_users.php");
+                            break;
+                            default:
+                                print "Bem vindo";
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+  </body>
+</html>
