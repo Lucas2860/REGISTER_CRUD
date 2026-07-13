cd6b055a15c4fd6fb2d303240c0b44c4343e0e25
Lucas2860 <lucas.santos286012@gmail.com>
2026-07-13 11:45:55 -0300

    :sparkles:feat: Config com o banco de dados

--- new_user.php
+++ new_user.php
@@ -1,1 +1,22 @@
 <h1>Novo usuário</h1>
+<form action='?action-page' method='POST'>
+    <div class="mb-3">
+        <label>Nome:</div>
+        <input type = "text" name="nome" class="form-control">    
+    </div>
+    <div class="mb-3">
+        <label>E-mail:</div>
+        <input type = "email" name="email" class="form-control">    
+    </div>
+    <div class="mb-3">
+        <label>Senha:</div>
+        <input type = "password" name="senha" class="form-control">    
+    </div>
+    <div class="mb-3">
+        <label>Data de Nascimento:</div>
+        <input type = "date" name="data_nasc" class="form-control">    
+    </div>
+    <div class="mb-3">
+        <input type = "submit" class="btn btn-primary">    
+    </div>
+<form>
